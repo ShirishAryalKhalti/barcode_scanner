c7d5134a66ebeed2fd7ca73b23279783e40c20a1
Shirish Aryal <shirish.aryal@khalti.com>
2024-12-17 16:34:29 +0545

    refactor NativeViewFactory to prepare for potential camera activity integration

--- android/src/main/kotlin/com/example/barcode_scanner/NativeViewFactory.kt
+++ android/src/main/kotlin/com/example/barcode_scanner/NativeViewFactory.kt
@@ -8,6 +8,7 @@ import io.flutter.plugin.platform.PlatformViewFactory
 class NativeViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
         val creationParams = args as Map<String?, Any?>?
-        return NativeView(context, viewId, creationParams)
+       return NativeView(context, viewId, creationParams)
+        // return CameraActivity()
     }
 }
